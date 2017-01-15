b24639572b0c2a2773037d3d6524d5c6faf3f4cf
US1549 <ur1597532@163.com>
2017-01-15 22:21:43 +0800

    起始主界面

---
+++ src/SearchGUI.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Administrator on 2017/1/15.
+ */
+public class SearchGUI extends Application{
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/StageGUI.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Administrator on 2017/1/15.
+ */
+public class StageGUI extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
